2c3d617219e419044dad7814d7008ba78b5d21f0
suchakree-petn <tutor1010@hotmail.com>
2025-06-03 04:03:16 +0700

    FEAT: Fetch data from Google Sheet

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+using PhEngine.GoogleSheets;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "GameConfig", fileName = "GameConfig", order = 0)]
+public class GameConfig : SerializedScriptableObject
+{
+    [FoldoutGroup("Setting", Expanded = false)]
+    [SerializeField] SheetDownloader downloader;
+
+    [FoldoutGroup("Setting", Expanded = false)]
+    [SerializeField] string spreadsheetId;
+    
+    [FoldoutGroup("Setting", Expanded = false)]
+    [SerializeField] string sheetNameAndRange;
+
+    public Dictionary<string, Config> Config;
+
+    [Button("Fetch Data", Icon = SdfIconType.ArrowCounterclockwise, ButtonHeight = 50)]
+    [PropertyOrder(-10)]
+    [GUIColor(0.5f, 0.8f, 0.5f)]
+    public void FetchDataFromGoogleSheet()
+    {
+        downloader
+            .CreateRequest<Config>(spreadsheetId, sheetNameAndRange)
+            .OnSuccess(list => Config = list.ToDictionary(config => config.Name, config => config))
+            .Download();
+    }
+
+
+
+}
+[Serializable]
+public class Config
+{
+    public string Name;
+    public int Amount;
+    public int Damage;
+    public int HP;
+    public float MissedChance;
+    public float Sec;
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
     [ShowInInspector]
     public GameContext GameContext { get; private set; }
+
+
     protected override void InitAfterAwake()
     {
-        GameContext = new();
     }
+    
+
 }
 
 [Serializable]
 public class GameContext
 {
     public GameMode GameMode;
+
+    [InlineEditor]
+    public GameConfig GameConfig;
 
 }
 
